40b37ca2708599b1408c432e7ce60b5ff2199e93
Adriel <adrielgusa@gmail.com>
2018-01-23 16:03:17 -0200

    Setting up webpack-dev-server

--- hello/src/index.jsx
+++ hello/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // JSX - JavaScript XML
-var template = <h1>Hello React! </h1>;
+var template = <h1>Hello React !!! </h1>;
 var appRoot = document.getElementById('root');
 
 ReactDOM.render(template, appRoot);
