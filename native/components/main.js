5a1bc7310c6ed887fc651a7e8aeea0a96dd38054
Adriel <adrielgusa@gmail.com>
2018-01-26 18:52:44 -0200

    Adding new file structure

---
+++ native/components/main.js
@@ -0,0 +1,24 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up Main.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
